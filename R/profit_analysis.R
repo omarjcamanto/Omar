6a166f2b4904d39a120409c120f198c6f81cafbe
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-09-05 13:38:59 -0400

    Fix: profit_analysis() has roxygen documentation and is exported. Closes #1.

--- R/profit_analysis.R
+++ R/profit_analysis.R
@@ -1,7 +1,24 @@
+#' Profit analysis
+#'
+#' @param data input data frame specifying price and acreage of each comparable lot.
+#' @param size a numerical vector specifying the acreage of lot of interest.
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' df <- dplyr::tribble(
+#' ~price, ~acres,
+#' 6969, 6,
+#' 6996, 9,
+#' 9669, 6,
+#' 9696, 9
+#' )
+#' profit_analysis(df)
 profit_analysis <- function(data, size = 1){
   data %>%
     dplyr::mutate(per_acre = price/acres) %>%
-    dplyr::summarise(retail = round(mean(per_acre, na.rm = TRUE), digits = 2),
+    dplyr::summarise("retail" = round(mean(per_acre, na.rm = TRUE), digits = 2),
               "high offer" = round(mean(per_acre, na.rm = TRUE), digits = 2)*.40,
               "middle offer" = round(mean(per_acre, na.rm = TRUE), digits = 2)*.35,
               "low offer" = round(mean(per_acre, na.rm = TRUE), digits = 2)*.30) %>%
