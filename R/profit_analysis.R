dabcee72973ed85149af816eba7af53a0ec43b07
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-09-05 17:04:16 -0400

    Feat: profit_analysis() can accommodate square-feet. Closes #4.

--- R/profit_analysis.R
+++ R/profit_analysis.R
@@ -8,22 +8,25 @@
 #'
 #' @examples
 #' df <- dplyr::tribble(
-#' ~price, ~acres,
-#' 6969, 6,
-#' 6996, 9,
-#' 9669, 6,
-#' 9696, 9
+#' ~price, ~size, ~unit,
+#' 6969, 6, "acre",
+#' 6996, 9, "sqft",
+#' 9669, 6, "acre",
+#' 9696, 9, "sqft"
 #' )
 #' profit_analysis(df)
 profit_analysis <- function(data, size = 1){
   data %>%
-    dplyr::mutate(per_acre = .data$price/.data$acres) %>%
+    dplyr::mutate(size = dplyr::case_when(.data$unit == "sqft" ~ .data$size/43560,
+                                          .data$unit == "acre" ~ .data$size),
+                  per_acre = .data$price/.data$size) %>%
     dplyr::summarise("retail" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2),
-              "high offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.40,
-              "middle offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.35,
-              "low offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.30) %>%
+                     "high offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.40,
+                     "middle offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.35,
+                     "low offer" = round(mean(.data$per_acre, na.rm = TRUE), digits = 2)*.30) %>%
     tidyr::pivot_longer("retail":"low offer",
-                 names_to = "guideline") %>%
+                        names_to = "guideline") %>%
     dplyr::mutate(!!glue::glue("{size}-acre lot") := .data$value*size) %>%
     dplyr::select(!.data$value)
 }
+
