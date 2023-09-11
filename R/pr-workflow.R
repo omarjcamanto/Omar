47adfd237f9f88877593514851b9547579f049bf
Algedonic <algedonic@Algedonics-MacBook-Air.local>
2023-09-11 16:10:42 -0400

    revisions

--- R/pr-workflow.R
+++ R/pr-workflow.R
@@ -3,11 +3,11 @@
 # Pre Work ---------------------------------------------------------------------
 pr_init(branch = "pr") #create temporary branch to record new work
 
-git_sitrep() #view the effects/side-effects of running pr_init().
+git_sitrep() #view the effects of running pr_init().
 #notice how the new branch is set up for new work to be recorded.
 
 # Work --------------------------------------------------------------------
-#record changes and commit the changes.
+#go to work and commit changes.
 
 # Post Work --------------------------------------------------------------------
 pr_push() #push local changes to GitHub fork (i.e., origin) and set up pull request to send the source repo (i.e., upstream)
