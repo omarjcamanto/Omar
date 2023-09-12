0b3317b5401f45e3368eb5051ac59a950f6358df
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-09-12 10:23:10 -0400

    added revisions

--- inst/pr-workflow.R
+++ inst/pr-workflow.R
@@ -1,6 +1,6 @@
-#pull request workflow in-code
+#pull request workflow in-code (from the contributor's perspective)
 
-# Pre Work ---------------------------------------------------------------------
+# Set up Pull-Request Branch ---------------------------------------------------------------------
 pr_init(branch = "pr") #create temporary branch to record new work
 
 git_sitrep() #view the effects of running pr_init().
@@ -9,10 +9,19 @@ git_sitrep() #view the effects of running pr_init().
 # Work --------------------------------------------------------------------
 #go to work and commit changes.
 
-# Post Work --------------------------------------------------------------------
+# Send out Pull-Request --------------------------------------------------------------------
 pr_push() #push local changes to GitHub fork (i.e., origin) and set up pull request to send the source repo (i.e., upstream)
 
-#if maintainer revises pull request use...
+#to view state of pull-request...
+pr_view()
+
+# Another Round of Revisions? ---------------------------------------------
+
+#if maintainer adds revisions pull request use...
 pr_pull() #pull changes from maintainer
 
+#if you add revisions to pull request use...
+pr_push() #send out pull-request again
+
+# Conclude Pull-Request ---------------------------------------------------
 pr_finish() #conclude pull request process.
