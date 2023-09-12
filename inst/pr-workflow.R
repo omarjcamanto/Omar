30965ff56c800d07a5056d69694218c2566caa46
Algedonic <algedonic@Algedonics-MacBook-Air.local>
2023-09-12 10:28:12 -0400

    cleaned some things up

--- inst/pr-workflow.R
+++ inst/pr-workflow.R
@@ -1,27 +1,27 @@
 #pull request workflow in-code (from the contributor's perspective)
 
 # Set up Pull-Request Branch ---------------------------------------------------------------------
-pr_init(branch = "pr") #create temporary branch to record new work
+usethis::pr_init(branch = "pr") #create temporary branch to record new work
 
-git_sitrep() #view the effects of running pr_init().
+usethis::git_sitrep() #view the effects of running pr_init().
 #notice how the new branch is set up for new work to be recorded.
 
 # Work --------------------------------------------------------------------
 #go to work and commit changes.
 
 # Send out Pull-Request --------------------------------------------------------------------
-pr_push() #push local changes to GitHub fork (i.e., origin) and set up pull request to send the source repo (i.e., upstream)
+usethis::pr_push() #push local changes to GitHub fork (i.e., origin) and set up pull request to send the source repo (i.e., upstream)
 
 #to view state of pull-request...
-pr_view()
+usethis::pr_view()
 
 # Another Round of Revisions? ---------------------------------------------
 
 #if maintainer adds revisions pull request use...
-pr_pull() #pull changes from maintainer
+usethis::pr_pull() #pull changes from maintainer
 
-#if you add revisions to pull request use...
-pr_push() #send out pull-request again
+#if you add revisions to pull request, commit revisions and use...
+usethis::pr_push() #send out pull-request again
 
 # Conclude Pull-Request ---------------------------------------------------
-pr_finish() #conclude pull request process.
+usethis::pr_finish() #conclude pull request process.
