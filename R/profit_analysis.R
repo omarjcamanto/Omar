c903b2ac913705e235b292d24d414ef680ebc23d
Algedonic <algedonic@Algedonics-MacBook-Air.local>
2023-10-01 16:53:42 -0400

    commit from pr1

--- R/profit_analysis.R
+++ R/profit_analysis.R
@@ -1,5 +1,6 @@
 #' Profit analysis
 #'
+#'line from pr1
 #'
 #' @param data input data frame specifying price and acreage of each comparable lot.
 #' @param size a numerical vector specifying the acreage of the lot of interest. 1 acre is the default.
